530034d1724eaafb614f44fec1390d524819b469
bta99 <tuananhvip1099@gmail.com>
2025-12-16 13:17:46 +0700

    Clean up deleted documentation files

--- disable-rls.sql
+++
@@ -1,9 +0,0 @@
--- Chỉ cần chạy lệnh này thôi:
-ALTER TABLE "Message" DISABLE ROW LEVEL SECURITY;
-
--- Kiểm tra xem đã tắt chưa:
-SELECT tablename, rowsecurity 
-FROM pg_tables 
-WHERE tablename = 'Message';
-
--- Kết quả phải là: rowsecurity = false
